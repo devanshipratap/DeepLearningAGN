3a0d50fb67b581136508c93ae99158e54f7b1242
Devanshi Pratap <dpratap2@illinois.edu>
2020-04-06 12:34:22 -0500

    pylinted neural_network.py

--- src/scripts/neural_network.py
+++ src/scripts/neural_network.py
@@ -1,44 +1,44 @@
 """
-script for training a neural network (resnet18) using pytorch
+Script for training a neural network (resnet18) using pytorch
 """
 
-import torch
+import os
+import sys
+import datetime
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils.data import Dataset
-from  torch.autograd import Variable
 import torch.optim as optim
-from torchvision import datasets, transforms
 import torchvision.models as models
-import lenstronomy.Util.image_util as image_util
-import os, sys
+import torch.utils.model_zoo as model_zoo
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import lenstronomy.Util.image_util as image_util
 from tqdm import tqdm
-import datetime
-import torch.utils.model_zoo as model_zoo
 from tensorboardX import SummaryWriter
+from torch.utils.data import Dataset
+from torch.autograd import Variable
+from torchvision import datasets, transforms
 
-
-### specify the data folder and hyperparameters for training
-folder = "./uncertainty_Full_train/"
+# Specify the data folder and hyper-parameters for training
+folder = "./full_train/"
 EPOCH = 60
 glo_batch_size = 10
 test_num_batch = 50
 
-normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 data_transform = transforms.Compose([
-            transforms.ToTensor(), # scale to [0,1] and convert to tensor
-            normalize,
-            ])
+    transforms.ToTensor(), # scale to [0,1] and convert to tensor
+    normalize, ])
 target_transform = torch.Tensor
 
 
-
-### using pytorch dataloader to read the data for training and testing
-class BHDataset(Dataset): # torch.utils.data.Dataset
+# Using pytorch dataloader to read the data for training and testing
+class BHDataset(Dataset):
+    """
+    Class to model training or testing data sets
+    """
     def __init__(self, root_dir, train=True, transform=None, target_transform=None):
         """
         Function to specify the path, and lookup tables (csv) for the dataset
@@ -54,19 +54,18 @@ class BHDataset(Dataset): # torch.utils.data.Dataset
         if self.train:
             self.path = os.path.join(self.root_dir, self.train_folder)
             self.df = pd.read_csv(self.path + 'train.csv')
-            self.df['M'] = self.df.M.mask(self.df.M == 0,8.8)
+            self.df['M'] = self.df.M.mask(self.df.M == 0, 8.8)
             #self.length = TRAINING_SAMPLES
         else:
             self.path = os.path.join(self.root_dir, self.test_folder)
             self.df = pd.read_csv(self.path + 'test.csv')
-            self.df['M'] = self.df.M.mask(self.df.M == 0,8.8)
+            self.df['M'] = self.df.M.mask(self.df.M == 0, 8.8)
             #self.length = TESTING_SAMPLES
 
     def __getitem__(self, index):
         """
         Function to obtain the data, and the labels in the given path
         """
-
         ID = self.df['ID'].iloc[[index]]
         M = self.df['M'].iloc[[index]]
         try:
@@ -86,49 +85,48 @@ class BHDataset(Dataset): # torch.utils.data.Dataset
         Function to get the length of the dataframe
         """
         return self.df.shape[0]
-        #return self.length
 
-### train loader load the data only from training set
-train_loader = torch.utils.data.DataLoader(BHDataset(folder, train=True, transform=data_transform, target_transform=target_transform),
-                    batch_size = glo_batch_size, shuffle = True
-                    )
+# Train loader loads the data only from training set
+train_loader = torch.utils.data.DataLoader(BHDataset(folder, train=True,
+                                                     transform=data_transform,
+                                                     target_transform=target_transform),
+                                           batch_size=glo_batch_size, shuffle=True)
 
 if __name__ == '__main__':
-
-
-    ### specify the number of output parameters (AGN mass only for example, so there is only one here)
+    # Specify the number of output parameters (AGN mass only for example, so there is only one here)
     dset_classes_number = 1
 
-    ### using a pretrained resnet18
+    # Using a pre-trained resnet18
     net = models.resnet18(pretrained=True)
 
-    ### modify the last layer of the network to output the parameters we wish to train
+    # Modify the last layer of the network to output the parameters we wish to train
     num_ftrs = net.fc.in_features
-    net.fc= nn.Linear(in_features=num_ftrs, out_features=dset_classes_number)
+    net.fc = nn.Linear(in_features=num_ftrs, out_features=dset_classes_number)
 
-    ### loss function for training
+    # Loss function for training
     loss_fn = nn.MESLoss(reduction='elementwise_mean')
 
-    ### putting the neural network on GPU
+    # Putting the neural network on GPU
     net.cuda()
 
-    ### specify the optimizer and the initial learning rate
-    optimizer = optim.Adam(net.parameters(), lr = 2*1e-4)
+    # Specify the optimizer and the initial learning rate
+    optimizer = optim.Adam(net.parameters(), lr=2*1e-4)
     tb = SummaryWriter()
 
-    ### set best_accuracy first, and overwrite it during training
+    # Set best_accuracy first, and overwrite it during training
     best_accuracy = float("inf")
 
-    ### looping through the training epoch. During each epoch, the neural network goes through the whole trianing set
+    # Looping through the training epoch.
+    # During each epoch, the neural network goes through the whole training set
     for epoch in range(EPOCH):
 
-        ### set the network to training phase
+        # Set the network to training phase
         net.train()
         total_loss = 0.0
         total_counter = 0
         total_rms = 0
 
-        for batch_idx, (data, target) in enumerate(tqdm(train_loader, total = len(train_loader))):
+        for batch_idx, (data, target) in enumerate(tqdm(train_loader, total=len(train_loader))):
             data, target = data.float(), target.float()
             data, target = Variable(data).cuda(), Variable(target).cuda()
 
@@ -151,27 +149,29 @@ if __name__ == '__main__':
         for i in range(len(avg_rms)):
             tb.add_scalar('rms %d' % (i+1), avg_rms[i])
 
-        # print test loss and tets rms
-        print(epoch, 'Train loss (averge per batch wise):', total_loss/(total_counter), ' RMS (average per batch wise):', np.array_str(avg_rms, precision=3))
+        # Print test loss and test RMS
+        print(epoch, 'Train loss (average per batch wise):', total_loss/(total_counter),
+              ' RMS (average per batch wise):', np.array_str(avg_rms, precision=3))
 
         with torch.no_grad():
 
-            ### set the network to evaluating phase
+            # Set the network to evaluating phase
             net.eval()
             total_loss = 0.0
             total_counter = 0
             total_rms = 0
 
-            ### test loader load the data only from test set
-            test_loader = torch.utils.data.DataLoader(BHDataset(folder, train=False, transform=data_transform, target_transform=target_transform),
-                        batch_size = glo_batch_size, shuffle = True
-                        )
+            # Test loader load the data only from test set
+            test_loader = torch.utils.data.DataLoader(BHDataset(folder, train=False,
+                                                                transform=data_transform,
+                                                                target_transform=target_transform),
+                                                      batch_size=glo_batch_size, shuffle=True)
 
             for batch_idx, (data, target) in enumerate(test_loader):
                 data, target = data.float(), target.float()
                 data, target = Variable(data).cuda(), Variable(target).cuda()
 
-                ### getting prediction from neural network
+                # Getting prediction from neural network
                 pred = net(data)
                 loss = loss_fn(pred, target)
                 square_diff = (pred - target)
@@ -190,10 +190,11 @@ if __name__ == '__main__':
             for i in range(len(avg_rms)):
                 tb.add_scalar('rms %d' % (i+1), avg_rms[i])
 
-            # print test loss and tets rms
-            print(epoch, 'Test loss (averge per batch wise):', total_loss/(total_counter), ' RMS (average per batch wise):', np.array_str(avg_rms, precision=3))
+            # print test loss and test RMS
+            print(epoch, 'Test loss (average per batch wise):', total_loss/(total_counter),
+                  'RMS (average per batch wise):', np.array_str(avg_rms, precision=3))
 
-            ### save the best fit models
+            # Save the best fit models
             if total_loss/(total_counter) < best_accuracy:
                 best_accuracy = total_loss/(total_counter)
                 datetime_today = str(datetime.date.today())
