19fb766b124819624172cb1e5337f8eaf2647cf5
Devanshi Pratap <dpratap2@illinois.edu>
2020-04-16 21:53:27 -0500

    pylint nn code and rename mass plot

--- src/scripts/neural_network.py
+++ src/scripts/neural_network.py
@@ -22,16 +22,16 @@ from torch.autograd import Variable
 from torchvision import datasets, transforms
 
 # Specify the data folder and hyper-parameters for training
-folder = "./full_train/"
+FOLDER = "./full_train/"
 EPOCH = 60
-glo_batch_size = 10
-test_num_batch = 50
+GLO_BATCH_SIZE = 10
+TEST_NUM_BATCH = 50
 
-normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-data_transform = transforms.Compose([
+NORMALIZE = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+DATA_TRANSFORM = transforms.Compose([
     transforms.ToTensor(), # scale to [0,1] and convert to tensor
-    normalize, ])
-target_transform = torch.Tensor
+    NORMALIZE, ])
+TARGET_TRANSFORM = torch.Tensor
 
 
 # Using pytorch dataloader to read the data for training and testing
@@ -87,52 +87,52 @@ class BHDataset(Dataset):
         return self.df.shape[0]
 
 # Train loader loads the data only from training set
-train_loader = torch.utils.data.DataLoader(BHDataset(folder, train=True,
-                                                     transform=data_transform,
-                                                     target_transform=target_transform),
-                                           batch_size=glo_batch_size, shuffle=True)
+TRAIN_LOADER = torch.utils.data.DataLoader(BHDataset(FOLDER, train=True,
+                                                     transform=DATA_TRANSFORM,
+                                                     target_transform=TARGET_TRANSFORM),
+                                           batch_size=GLO_BATCH_SIZE, shuffle=True)
 
 if __name__ == '__main__':
     # Specify the number of output parameters (AGN mass only for example, so there is only one here)
-    dset_classes_number = 1
+    DSET_CLASSES_NUMBER = 1
 
     # Using a pre-trained resnet18
-    net = models.resnet18(pretrained=True)
+    NET = models.resnet18(pretrained=True)
 
     # Modify the last layer of the network to output the parameters we wish to train
-    num_ftrs = net.fc.in_features
-    net.fc = nn.Linear(in_features=num_ftrs, out_features=dset_classes_number)
+    NUM_FTRS = NET.fc.in_features
+    NET.fc = nn.Linear(in_features=NUM_FTRS, out_features=DSET_CLASSES_NUMBER)
 
     # Loss function for training
-    loss_fn = nn.MESLoss(reduction='elementwise_mean')
+    LOSS_FN = nn.MESLoss(reduction='elementwise_mean')
 
     # Putting the neural network on GPU
-    net.cuda()
+    NET.cuda()
 
     # Specify the optimizer and the initial learning rate
-    optimizer = optim.Adam(net.parameters(), lr=2*1e-4)
-    tb = SummaryWriter()
+    OPTIMIZER = optim.Adam(NET.parameters(), lr=2*1e-4)
+    TB = SummaryWriter()
 
     # Set best_accuracy first, and overwrite it during training
-    best_accuracy = float("inf")
+    BEST_ACCURACY = float("inf")
 
     # Looping through the training epoch.
     # During each epoch, the neural network goes through the whole training set
     for epoch in range(EPOCH):
 
         # Set the network to training phase
-        net.train()
+        NET.train()
         total_loss = 0.0
         total_counter = 0
         total_rms = 0
 
-        for batch_idx, (data, target) in enumerate(tqdm(train_loader, total=len(train_loader))):
+        for batch_idx, (data, target) in enumerate(tqdm(TRAIN_LOADER, total=len(TRAIN_LOADER))):
             data, target = data.float(), target.float()
             data, target = Variable(data).cuda(), Variable(target).cuda()
 
-            optimizer.zero_grad()
-            output = net(data)
-            loss = loss_fn(output, target)
+            OPTIMIZER.zero_grad()
+            output = NET(data)
+            loss = LOSS_FN(output, target)
 
             square_diff = (output - target) #((output - target)**2)**(0.5)
             total_rms += square_diff.std(dim=0)
@@ -140,14 +140,14 @@ if __name__ == '__main__':
             total_counter += 1
 
             loss.backward()
-            optimizer.step()
+            OPTIMIZER.step()
 
         # Collect RMS over each label
         avg_rms = total_rms / (total_counter)
         avg_rms = avg_rms.cpu()
         avg_rms = (avg_rms.data).numpy()
         for i in range(len(avg_rms)):
-            tb.add_scalar('rms %d' % (i+1), avg_rms[i])
+            TB.add_scalar('rms %d' % (i+1), avg_rms[i])
 
         # Print test loss and test RMS
         print(epoch, 'Train loss (average per batch wise):', total_loss/(total_counter),
@@ -156,31 +156,31 @@ if __name__ == '__main__':
         with torch.no_grad():
 
             # Set the network to evaluating phase
-            net.eval()
+            NET.eval()
             total_loss = 0.0
             total_counter = 0
             total_rms = 0
 
             # Test loader load the data only from test set
-            test_loader = torch.utils.data.DataLoader(BHDataset(folder, train=False,
-                                                                transform=data_transform,
-                                                                target_transform=target_transform),
-                                                      batch_size=glo_batch_size, shuffle=True)
+            test_loader = torch.utils.data.DataLoader(BHDataset(FOLDER, train=False,
+                                                                transform=DATA_TRANSFORM,
+                                                                target_transform=TARGET_TRANSFORM),
+                                                      batch_size=GLO_BATCH_SIZE, shuffle=True)
 
             for batch_idx, (data, target) in enumerate(test_loader):
                 data, target = data.float(), target.float()
                 data, target = Variable(data).cuda(), Variable(target).cuda()
 
                 # Getting prediction from neural network
-                pred = net(data)
-                loss = loss_fn(pred, target)
+                pred = NET(data)
+                loss = LOSS_FN(pred, target)
                 square_diff = (pred - target)
                 total_rms += square_diff.std(dim=0)
                 total_loss += loss.item()
                 total_counter += 1
 
-                if batch_idx % test_num_batch == 0 and batch_idx != 0:
-                    tb.add_scalar('test_loss', loss.item())
+                if batch_idx % TEST_NUM_BATCH == 0 and batch_idx != 0:
+                    TB.add_scalar('test_loss', loss.item())
                     break
 
             # Collect RMS over each label
@@ -188,17 +188,17 @@ if __name__ == '__main__':
             avg_rms = avg_rms.cpu()
             avg_rms = (avg_rms.data).numpy()
             for i in range(len(avg_rms)):
-                tb.add_scalar('rms %d' % (i+1), avg_rms[i])
+                TB.add_scalar('rms %d' % (i+1), avg_rms[i])
 
             # print test loss and test RMS
             print(epoch, 'Test loss (average per batch wise):', total_loss/(total_counter),
                   'RMS (average per batch wise):', np.array_str(avg_rms, precision=3))
 
             # Save the best fit models
-            if total_loss/(total_counter) < best_accuracy:
-                best_accuracy = total_loss/(total_counter)
+            if total_loss/(total_counter) < BEST_ACCURACY:
+                BEST_ACCURACY = total_loss/(total_counter)
                 datetime_today = str(datetime.date.today())
-                torch.save(net, './saved_model/' + datetime_today +'resnet18.mdl')
+                torch.save(NET, './saved_model/' + datetime_today +'resnet18.mdl')
                 print("saved to " + datetime_today + "resnet18.mdl" + " file.")
 
-    tb.close()
+    TB.close()
