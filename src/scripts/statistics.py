3f3a0f2fd7d8c6a49948f7effac480dffce28b26
Devanshi Pratap <dpratap2@illinois.edu>
2020-05-26 19:19:46 -0700

    fixed stats script

--- src/scripts/statistics.py
+++ src/scripts/statistics.py
@@ -18,15 +18,17 @@ DF = DF[(DF.Mass_ground_truth != 0)]
 PEARSON1 = stats.pearsonr(DF['z_ground_truth'], DF['Mass_ground_truth'])
 PEARSON2 = stats.pearsonr(DF['z_ground_truth'], DF['Mass_prediction'])
 PEARSON3 = stats.pearsonr(DF['z_prediction'], DF['Mass_prediction'])
-PEARSON5 = stats.pearsonr(DF['z_ground_truth'], DF['z_prediction'])
 PEARSON4 = stats.pearsonr(DF['Mass_ground_truth'], DF['Mass_prediction'])
+PEARSON5 = stats.pearsonr(DF['z_ground_truth'], DF['z_prediction'])
+PEARSON6 = stats.pearsonr(DF['Mass_ground_truth'], DF['z_prediction'])
 
-# We only want the coefficient at the 0th index, [1] returns p value
-print('Pearson statistics for z ground truth vs mass ground truth: ', PEARSON1[0])
-print('Pearson statistics for z ground truth vs mass prediction: ', PEARSON2[0])
-print('Pearson statistics for z prediction vs mass prediction: ', PEARSON3[0])
-print('Pearson statistics for mass ground truth vs mass prediction: ', PEARSON4[0])
-print('Pearson statistics for z ground truth vs z prediction: ', PEARSON5[0])
+# [0] contains the coefficient, [1] returns p value
+print('Pearson statistics for mass ground truth vs z ground truth: ', PEARSON1)
+print('Pearson statistics for mass prediction vs z ground truth: ', PEARSON2)
+print('Pearson statistics for mass prediction vs z prediction: ', PEARSON3)
+print('Pearson statistics for mass prediction vs mass ground truth: ', PEARSON4)
+print('Pearson statistics for z prediction vs z ground truth: ', PEARSON5)
+print('Pearson statistics for z prediction vs mass ground truth: ', PEARSON6)
 
 ########## SPEARMAN STATISTICS ##########
 
@@ -34,11 +36,14 @@ print('Pearson statistics for z ground truth vs z prediction: ', PEARSON5[0])
 SPEARMAN1 = stats.spearmanr(DF['z_ground_truth'], DF['Mass_ground_truth'])
 SPEARMAN2 = stats.spearmanr(DF['z_ground_truth'], DF['Mass_prediction'])
 SPEARMAN3 = stats.spearmanr(DF['z_prediction'], DF['Mass_prediction'])
-SPEARMAN4 = stats.spearmanr(DF['z_ground_truth'], DF['z_prediction'])
-SPEARMAN5 = stats.spearmanr(DF['Mass_ground_truth'], DF['Mass_prediction'])
+SPEARMAN4 = stats.spearmanr(DF['Mass_ground_truth'], DF['Mass_prediction'])
+SPEARMAN5 = stats.spearmanr(DF['z_ground_truth'], DF['z_prediction'])
+SPEARMAN6 = stats.pearsonr(DF['Mass_ground_truth'], DF['z_prediction'])
 
-print('Spearman statistics for z ground truth vs mass ground truth: ', SPEARMAN1[0])
-print('Spearman statistics for z ground truth vs mass prediction: ', SPEARMAN2[0])
-print('Spearman statistics for z prediction vs mass prediction: ', SPEARMAN2[0])
-print('Spearman statistics for mass ground truth vs mass prediction: ', SPEARMAN4[0])
-print('Spearman statistics for z ground truth vs z prediction: ', SPEARMAN5[0])
+# [0] contains the coefficient, [1] returns p value
+print('Spearman statistics for mass ground truth vs z ground truth: ', SPEARMAN1)
+print('Spearman statistics for mass prediction vs z ground truth: ', SPEARMAN2)
+print('Spearman statistics for mass prediction vs z prediction: ', SPEARMAN3)
+print('Spearman statistics for mass prediction vs mass ground truth: ', SPEARMAN4)
+print('Spearman statistics for z prediction vs z ground truth: ', SPEARMAN5)
+print('Spearman statistics for z prediction vs mass ground truth: ', SPEARMAN6)
